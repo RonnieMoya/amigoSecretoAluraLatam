1db44d892d9a99236c3b5a346ed070de0f46b817
Ronnie Moya <ronnie.moya@inacapmail.cl>
2025-02-23 22:43:30 -0300

    Funcion Sortear Amigo

--- app.js
+++ app.js
@@ -14,15 +14,12 @@ function agregarAmigo() {
     }
     else {
         listaAmigos.push(ingresoAmigo.value);// agrega amigos a la lista
-        console.log(`Amigo ${ingresoAmigo.value} agregado a la lista.`);
         actualizarListaAmigos()
 
     }
     ingresoAmigo.value = "";
     return;
 }
-
-
 function actualizarListaAmigos() {
          
     // Limpiar la lista existente
@@ -37,4 +34,26 @@ function actualizarListaAmigos() {
         // Agregar el elemento de lista a la lista en pantalla
         ulListaAmigos.appendChild(li);
     }
+}
+
+function sortearAmigo() {
+    
+    // Validar que haya amigos disponibles
+    if (listaAmigos.length === 0) {
+        alert("Lista de amigos vacía, por favor ingresa los nombres antes de sortear.");
+    } else {
+        // Generar un índice aleatorio
+        const indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        
+        // Obtener el nombre sorteado
+        let amigoSorteado = listaAmigos[indiceAleatorio];
+        
+        // Mostrar el resultado
+        document.getElementById("resultado").innerHTML = `Tú amigo secreto es: ${amigoSorteado}`;
+        listaAmigos.length = 0;// deja la lista vacia
+        ulListaAmigos.innerHTML = ""; // limpia la pantalla de la lista de amigos
+        
+    }
+    
+    return;
 }
